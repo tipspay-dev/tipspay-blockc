7ec08156c91d11b62bc38c1af0362d5d42172d9b
tipspay-dev <developer@tipspay.org>
2026-01-28 18:28:47 +0300

    Update cloudfront.tf

--- infra/terraform/cloudfront.tf
+++ infra/terraform/cloudfront.tf
@@ -1,1 +1,39 @@
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "${var.project_name}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "frontend_distribution" {
+  enabled             = true
+  price_class         = var.cloudfront_price_class
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id   = "frontend-s3"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "frontend-s3"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
 
