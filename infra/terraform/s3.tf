bae8a06a54cf615dd259881c8638284c05bb732f
tipspay-dev <developer@tipspay.org>
2026-01-28 18:27:55 +0300

    Update s3.tf

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -1,1 +1,22 @@
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = var.frontend_bucket_name
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+}
 
