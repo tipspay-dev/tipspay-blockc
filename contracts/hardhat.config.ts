c3c6da05de4e657447a3eb7c2fc5f86d336b17e3
tipspay-dev <developer@tipspay.org>
2026-01-28 17:33:16 +0300

    Update hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,1 +1,15 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    managedBlockchain: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
+
+export default config;
 
