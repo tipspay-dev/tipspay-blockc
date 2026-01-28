e72b62ef82714b506a940e767bb1486d5f0d3ce6
tipspay-dev <developer@tipspay.org>
2026-01-28 18:23:33 +0300

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,1 +1,16 @@
+output "frontend_bucket" {
+  value = aws_s3_bucket.frontend_bucket.bucket
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend_distribution.id
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.backend_repo.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.blockc_cluster.name
+}
 
