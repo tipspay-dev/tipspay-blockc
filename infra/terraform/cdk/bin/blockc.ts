a36391417c189721a9884a7af3b344217fb46d36
Tipspay Architect <developer@tipspay.org>
2026-01-28 21:11:37 +0300

    Update blockc.ts

--- infra/terraform/cdk/bin/blockc.ts
+++ infra/terraform/cdk/bin/blockc.ts
@@ -1,1 +1,12 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { TipspayBlockcStack } from "../lib/tipspay-blockc-stack";
+
+const app = new cdk.App();
+
+new TipspayBlockcStack(app, "TipspayBlockcStack", {
+  env: {
+    region: "us-east-1"
+  }
+});
 
