3cf4dbf9b6f0614b4328d45200e60d0bffb1e97d
tipspay-dev <developer@tipspay.org>
2026-01-28 17:45:40 +0300

    Update env.ts

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,1 +1,7 @@
+export default {
+  RPC_URL: process.env.RPC_URL!,
+  HOSTING_CONTRACT: process.env.HOSTING_CONTRACT!,
+  WEBHOOK_SECRET: process.env.WEBHOOK_SECRET!,
+  WEBHOOK_URL: process.env.WEBHOOK_URL!
+};
 
