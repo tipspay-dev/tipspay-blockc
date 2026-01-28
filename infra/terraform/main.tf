b3b140f2ea7464034349b619014276f591d5d1ae
tipspay-dev <developer@tipspay.org>
2026-01-28 18:24:24 +0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,1 +1,24 @@
+module "vpc" {
+  source = "./vpc.tf"
+}
+
+module "ecr" {
+  source = "./ecr.tf"
+}
+
+module "ecs" {
+  source = "./ecs.tf"
+}
+
+module "s3" {
+  source = "./s3.tf"
+}
+
+module "cloudfront" {
+  source = "./cloudfront.tf"
+}
+
+module "managed_blockchain" {
+  source = "./managed_blockchain.tf"
+}
 
