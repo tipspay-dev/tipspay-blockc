07db115facf1142bc0046bf31ab0a34599482917
tipspay-dev <developer@tipspay.org>
2026-01-28 18:22:35 +0300

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,30 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "tipspay-blockc"
+}
+
+variable "frontend_bucket_name" {
+  type        = string
+  default     = "tipspay-blockc-frontend"
+}
+
+variable "cloudfront_price_class" {
+  type        = string
+  default     = "PriceClass_100"
+}
+
+variable "ecs_instance_type" {
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "ecs_desired_count" {
+  type        = number
+  default     = 1
+}
 
