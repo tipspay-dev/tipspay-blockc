4fa77ce4f758b4d13ce2f50ab81d8f24d8719b5a
tipspay-dev <developer@tipspay.org>
2026-01-28 18:29:45 +0300

    Update managed_blockchain.tf

--- infra/terraform/managed_blockchain.tf
+++ infra/terraform/managed_blockchain.tf
@@ -1,1 +1,14 @@
+resource "aws_managedblockchain_node" "eth_node" {
+  network_id = "n-ethereum-mainnet"
+  member_id  = "m-1234567890"
+
+  node_configuration {
+    instance_type = "bc.t3.large"
+    availability_zone = "${var.aws_region}a"
+  }
+}
+
+output "managed_blockchain_rpc" {
+  value = "https://your-managed-blockchain-rpc-url"
+}
 
